91d2a2c3d947402d9e94e61c59aa4dc10f221d83
Андрей Брижак <andrewbrizhak@gmail.com>
2020-07-01 17:55:45 +0800

    add class

---
+++ MailSender/Model/EmailSendServiceClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    class EMailInfo
+    {
+        public string SMTPClient { get; set; }
+        public int  Port { get; set; }
+        public  string  Subject { get; set; }
+        public string Body { get; set; }
+        public string Sender { get; set; }
+        public string Password { get;   set; }
+        public string From { get; set; }
+        public string To { get; set; }
+
+    }
+    class EmailSendServiceClass
+    {
+    }
+}
